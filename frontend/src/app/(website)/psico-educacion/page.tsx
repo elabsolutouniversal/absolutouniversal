fb460cc57d997f8e21cc8567e482fc8c382d15fa
Carlos Elio <carloseliogonzalezapa1997@gmail.com>
2025-07-25 22:40:11 -0500

    Blog PsicoEducacion

--- frontend/src/app/(website)/psico-educacion/page.tsx
+++ frontend/src/app/(website)/psico-educacion/page.tsx
@@ -1,7 +1,14 @@
+import BlogList from '@/components/blog/BlogList'
 import React from 'react'
+
+import { blogPosts } from '@/data/servicios/blog';
+
+
 
 export default function page() {
   return (
-    <div>page</div>
+    <main>
+      <BlogList posts={blogPosts} postsPerPage={6} />
+    </main>
   )
 }
